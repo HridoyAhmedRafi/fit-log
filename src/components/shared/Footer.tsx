65ce157756665dd7491636e79fdc64854f6e91bc
Hridoy-Ahmed-Rafi20 <hridoyahmedrafi20@gmail.com>
2026-09-24 12:45:48 +0600

    footer section complete

---
+++ src/components/shared/Footer.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import footerLogo from "@/assets/footer_logo.png";
+
+const Footer = () => {
+  return (
+    <div className="bg-[#090A0D] border-t bordert-[#1A1D24]">
+      <div className="flex items-center justify-between container mx-auto px-4 py-8">
+        <div className="flex items-center justify-between gap-2">
+          <Image src={footerLogo} alt="footer logo"></Image>
+          <h2 className="text-white font-bold">FITLOG</h2>
+        </div>
+
+        <div>
+          <p className="text-[#6B7280] text-[12px]">
+            © 2026 FitLog — Workout Library. Train hard, log honest.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
