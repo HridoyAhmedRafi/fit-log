e90d49a8b07237f47993b451e3159e80bb6dd1e8
Hridoy-Ahmed-Rafi20 <hridoyahmedrafi20@gmail.com>
2026-09-26 00:17:06 +0600

    the library section responsive complete

--- src/components/homepage/TheLibrary.tsx
+++ src/components/homepage/TheLibrary.tsx
@@ -13,12 +13,14 @@ const TheLibrary = async () => {
   return (
     <div className="mt-10">
       <div>
-        <h1 className="text-white text-[30px] font-bold">THE LIBRARY</h1>
-        <p className="text-[#9CA3AF] text-[14px] font-semibold">
+        <h1 className="text-white text-[18px] md:text-[30px] font-bold">
+          THE LIBRARY
+        </h1>
+        <p className="text-[#9CA3AF] text-[11px] md:text-[14px] font-semibold">
           Twelve lifts covering every major muscle group.
         </p>
       </div>
-      <div id="library" className="grid grid-cols-3 gap-5 mt-7">
+      <div id="library" className="grid grid-cols-1 md:grid-cols-3 gap-5 mt-7">
         {AllExercise.map((exercise: IExercise) => (
           <ExerciseCard exercise={exercise} key={exercise.id}></ExerciseCard>
         ))}
