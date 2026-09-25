ff049c499a9cc202c7fdf746f40a698ab42eee55
Hridoy-Ahmed-Rafi20 <hridoyahmedrafi20@gmail.com>
2026-09-26 00:18:45 +0600

    footer section responsive complete

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -4,14 +4,14 @@ import footerLogo from "@/assets/footer_logo.png";
 const Footer = () => {
   return (
     <div className="bg-[#090A0D] border-t bordert-[#1A1D24]">
-      <div className="flex items-center justify-between container mx-auto px-4 py-8">
+      <div className="flex flex-col md:flex-row items-center justify-between container mx-auto px-4 py-8">
         <div className="flex items-center justify-between gap-2">
           <Image src={footerLogo} alt="footer logo"></Image>
           <h2 className="text-white font-bold">FITLOG</h2>
         </div>
 
         <div>
-          <p className="text-[#6B7280] text-[12px]">
+          <p className="text-[#6B7280] text-[12px] text-center">
             © 2026 FitLog — Workout Library. Train hard, log honest.
           </p>
         </div>
